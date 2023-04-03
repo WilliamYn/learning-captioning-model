99f6264493f54316bc276f09f49b5061aaae2896
jehag <jeremy.hage@polymtl.ca>
2023-04-03 15:33:38 -0400

    added main

--- learning_model/download_from_azure.py
+++ learning_model/download_from_azure.py
@@ -9,16 +9,16 @@ captions_txt_blob_name = 'custom_captions.txt'
 container_client = blob_service_client.get_container_client(container_name)
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=captions_txt_blob_name)
 
-def get_azure_custom_captions_txt_file():
-    captions_txt_content = blob_client.download_blob().content_as_text()
-    with open("data/textFiles/custom_captions.txt", "w") as f:
-        f.write(captions_txt_content)
-
 DATA_DIR = "data"
 IMAGES_DIR = os.path.join(DATA_DIR, "Images")
 class BlobDownloader:
   def __init__(self, container_client):
     self.my_container = container_client
+
+  def get_azure_custom_captions_txt_file():
+    captions_txt_content = blob_client.download_blob().content_as_text()
+    with open("data/textFiles/custom_captions.txt", "w") as f:
+        f.write(captions_txt_content)
 
   def save_blob(self, file_name, file_content):
     # Get full path to the file
@@ -41,3 +41,11 @@ class BlobDownloader:
       
       # Save the blob in the data/Images directory
       self.save_blob(blob.name, bytes)
+
+def main():
+  downloader = BlobDownloader(container_client)
+  downloader.get_azure_custom_captions_txt_file()
+  downloader.download_all_blobs_in_container()
+
+if __name__ == "__main__":
+  main()
