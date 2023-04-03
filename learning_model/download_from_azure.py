68a791cb5ff753430522ecd9725d65bc52225af2
jehag <jeremy.hage@polymtl.ca>
2023-04-03 15:47:29 -0400

    bug fix

--- learning_model/download_from_azure.py
+++ learning_model/download_from_azure.py
@@ -9,7 +9,10 @@ captions_txt_blob_name = 'custom_captions.txt'
 container_client = blob_service_client.get_container_client(container_name)
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=captions_txt_blob_name)
 
-DATA_DIR = "data"
+# Get the absolute path of the directory where the script is located
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Define the data directory using the absolute path
+DATA_DIR = os.path.join(SCRIPT_DIR, "data")
 IMAGES_DIR = os.path.join(DATA_DIR, "Images")
 class BlobDownloader:
   def __init__(self, container_client):
@@ -17,8 +20,11 @@ class BlobDownloader:
 
   def get_azure_custom_captions_txt_file(self):
     captions_txt_content = blob_client.download_blob().content_as_text()
-    with open("data/textFiles/custom_captions.txt", "w") as f:
+    text_files_dir = os.path.join(DATA_DIR, "textFiles")
+    os.makedirs(text_files_dir, exist_ok=True)  # create the directory if it doesn't exist
+    with open(os.path.join(text_files_dir, "custom_captions.txt"), "w") as f:
         f.write(captions_txt_content)
+
 
   def save_blob(self, file_name, file_content):
     # Get full path to the file
