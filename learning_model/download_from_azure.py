b5ae5de78c34c470a9aacee8d03005df9d0bd72b
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2023-04-03 13:11:55 -0400

    Update download_from_azure.py

--- learning_model/download_from_azure.py
+++ learning_model/download_from_azure.py
@@ -14,23 +14,30 @@ def get_azure_custom_captions_txt_file():
     with open("data/textFiles/custom_captions.txt", "w") as f:
         f.write(captions_txt_content)
 
-def save_blob(file_name, file_content):
+DATA_DIR = "data"
+IMAGES_DIR = os.path.join(DATA_DIR, "Images")
+class BlobDownloader:
+  def __init__(self, container_client):
+    self.my_container = container_client
+
+  def save_blob(self, file_name, file_content):
     # Get full path to the file
-    
-    download_file_path = os.path.join("data/Images", file_name)
+    download_file_path = os.path.join(IMAGES_DIR, file_name)
 
     # for nested blobs, create local path as well!
     os.makedirs(os.path.dirname(download_file_path), exist_ok=True)
 
     with open(download_file_path, "wb") as file:
-        file.write(file_content)
+      file.write(file_content)
 
-get_azure_custom_captions_txt_file()
-my_blobs = container_client.list_blobs()
-for blob in my_blobs:
-    print(blob.name)
-    bytes = container_client.get_blob_client(blob).download_blob().readall()
-
-
-
-
+  def download_all_blobs_in_container(self):
+    my_blobs = self.my_container.list_blobs()
+    for blob in my_blobs:
+      if blob.name == "custom_captions.txt":
+        continue
+      
+      print(blob.name)
+      bytes = self.my_container.get_blob_client(blob).download_blob().readall()
+      
+      # Save the blob in the data/Images directory
+      self.save_blob(blob.name, bytes)
